73b42d366f1e7a2d986cb19941c20ccac4bcd684
Rowan Christie (RIT Student) <smc9227@ad.rit.edu>
2020-02-29 18:20:34 -0500

    created file for creating newick trees

--- create-newick-tree_02292020.R
+++ create-newick-tree_02292020.R
@@ -1,3 +1,86 @@
+# Feb 29 2020
+# File including functions for creating a phylogentic tree in
+# Newick format
+# Defaults are:
+
+#_____________________
+# Load Libraries
+
+library(ape)
+library(phangorn)
+library(seqinr)
+library(BiocManager)
+library(Biostrings)
+library(msa)
+library(ggplot2)
+library(ggtree)
+
+#_____________________
+#1st Step: Perform MSA
+
+#Function for performing msa
+msa_data <- function(path_to_seq, format = "fasta") {
+  #call to compiled fasta file with all of the bacterial sequences
+  seq <- readDNAStringSet(path_to_seq, format = format)
+  #call MSA function which performs multiple sequence alignment
+  alignment <- msa(seq)
+  return(alignment)
+}
+
+#works!
+msa_test <- msa_data("C:/Users/smc9227/Documents/bioinformatics_code_stuff/antibiotic_resistant_strains3.fasta", "fasta")
+
+#_____________________
+
+#2nd Step: Convert the MSA to PhyDat class so it can be used in a tree
+
+convert_msa_to_phyDat <- function(alignment) {
+  msa_phyDat <- msaConvert(alignment, type="phangorn::phyDat")
+  return(msa_phyDat)
+}
+
+phyDat_test <- convert_msa_to_phyDat(msa_test)
+
+
+
+#master function for creating newick tree
+create_phyDat <- function(path, model) {
+  the_msa <- msa_data(path)
+  the_phyDat <- convert_msa_to_phyDat(the_msa)
+  
+  #right now we only have phyDat function so far so return that
+  return(the_phyDat)
+}
+
+path <- "C:/Users/smc9227/Documents/bioinformatics_code_stuff/antibiotic_resistant_strains3.fasta"
+
+create_phyDat(path)
+
+
+
+
+#perform conversion function to phyDat class so phangorn can operate on it
+bact_phyDat <- msaConvert(bact_alignment, type="phangorn::phyDat")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------
+
+# PYLOGENETIC TREE EXAMPLE DEC 19 2020
+
 #---------------------------------------------------------
 #Process to create a phylogenetic tree for
 #bacterial genes with TetM gene
